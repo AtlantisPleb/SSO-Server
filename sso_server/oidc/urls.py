24a6de961eef348aaff62f1dc4a941c7219634e4
Christopher David <chris@openagents.com>
2024-09-17 02:27:57 -0500

    Added 'userinfo' endpoint to urls.py for OIDC user information retrieval.

--- sso_server/oidc/urls.py
+++ sso_server/oidc/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('authorize/', views.authorize, name='oidc_authorize'),
     path('token/', views.token, name='oidc_token'),
+    path('userinfo/', views.userinfo, name='oidc_userinfo'),
 ]
