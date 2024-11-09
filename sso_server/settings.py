69475b316203ccfadb6bcac79adf905ed239fa5b
Christopher David <chris@openagents.com>
2024-11-08 21:08:05 -0600

    fix

--- sso_server/settings.py
+++ sso_server/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'connect_timeout': 10,
         },
-        'CONN_MAX_AGE': 600, 
+        'CONN_MAX_AGE': 600,
     }
 }
 
@@ -131,7 +131,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'sso_app', 'static'),
