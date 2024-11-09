d9c9c34b43ce27c1a2cc0439edfcfc5331bd6437
Christopher David <chris@openagents.com>
2024-11-08 20:54:17 -0600

    Update STATICFILES_DIRS path in production settings to reflect new static files location.

--- sso_server/production.py
+++ sso_server/production.py
@@ -20,26 +20,20 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'sso_app/static'),
 ]
 
 # Add whitenoise middleware after security middleware
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
-# Enable WhiteNoise's GZip compression of static assets
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Simplified static file serving
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Security settings
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-
-# Make sure you have this for serving static files
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-]
 
 # Logging configuration
 LOGGING = {
